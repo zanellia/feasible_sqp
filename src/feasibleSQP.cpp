3827020ee67e9666cb6fb3dd8c40b5649f4e8ffc
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2020-08-25 14:47:05 +0200

    add calls to casadi functions

--- src/feasibleSQP.cpp
+++ src/feasibleSQP.cpp
@@ -37,15 +37,104 @@
 
 #include <qpOASES.hpp>
 #include "qrecipe_data.hpp"
-// #include <casadi/casadi.hpp>
-// using namespace casadi;
-// using namespace std;
+#include <casadi/casadi.hpp>
+#include <casadi/casadi_c.h>
+#include "ca_dfdy.h"
+#include "ca_dgdy.h"
+#include "ca_g.h"
+#include "ca_dLdyy.h"
 
+using namespace casadi;
+using namespace std;
 
+void usage_cplusplus(){
+  cout << "---" << endl;
+  cout << "Usage from CasADi C++:" << endl;
+  cout << endl;
+
+  // Use CasADi's "external" to load the compiled function
+  Function f = external("f");
+
+  // Use like any other CasADi function
+  vector<double> x = {1, 2, 3, 4};
+  vector<DM> arg = {reshape(DM(x), 2, 2), 5};
+  vector<DM> res = f(arg);
+
+  cout << "result (0): " << res.at(0) << endl;
+  cout << "result (1): " << res.at(1) << endl;
+}
 
 int main( )
 {
+    #define nv 2
+    #define ni 1
+
+    Function ca_dfdy = external("ca_dfdy");
+    vector<double> y = {1, 2};
+    vector<double> lam = {1};
+    vector<DM> arg = {reshape(DM(y), 2, 1)};
+    vector<DM> dfdy_eval = ca_dfdy(arg);
+    cout << "result (0): " << dfdy_eval.at(0) << endl;
+
+    Function ca_g = external("ca_g");
+    vector<DM> g_eval = ca_g(arg);
+    cout << "result (0): " << g_eval.at(0) << endl;
+
+    Function ca_dgdy = external("ca_dgdy");
+    vector<DM> dgdy_eval = ca_dgdy(arg);
+    cout << "result (0): " << dgdy_eval.at(0) << endl;
+
+    Function ca_dLdyy = external("ca_dLdyy");
+    arg = {reshape(DM(y), nv, 1), reshape(DM(lam), ni, 1)};
+    vector<DM> dLdyy_eval = ca_dLdyy(arg);
+    cout << "result (0): " << dLdyy_eval.at(0) << endl;
+
 	USING_NAMESPACE_QPOASES
+
+    // get sparsity patterns
+    casadi_int n_in = ca_dgdy_n_in();
+    casadi_int n_out = ca_dgdy_n_out();
+
+    casadi_int sz_arg=n_in, sz_res=n_out, sz_iw=0, sz_w=0;
+
+    ca_dgdy_work(&sz_arg, &sz_res, &sz_iw, &sz_w);
+    printf("Work vector sizes:\n");
+    printf("sz_arg = %lld, sz_res = %lld, sz_iw = %lld, sz_w = %lld\n\n",
+           sz_arg, sz_res, sz_iw, sz_w);
+
+    casadi_int nnz; 
+    casadi_int ncol; 
+    /* Print the sparsities of the inputs and outputs */
+    casadi_int ca_i;
+    // skip inputs
+    ca_i = n_in;
+    // Retrieve the sparsity pattern - CasADi uses column compressed storage (CCS)
+    const casadi_int *sp_i;
+    printf("Output %lld\n", ca_i-n_in);
+    sp_i = ca_dgdy_sparsity_out(ca_i-n_in);
+    if (sp_i==0) return 1;
+    casadi_int nrow = *sp_i++; /* Number of rows */
+    ncol = *sp_i++; /* Number of columns */
+    const casadi_int *colind = sp_i; /* Column offsets */
+    const casadi_int *row = sp_i + ncol+1; /* Row nonzero */
+    nnz = sp_i[ncol]; /* Number of nonzeros */
+
+    sparse_int_t A_ir[nnz];
+    sparse_int_t A_jc[ncol];
+    sparse_int_t A_val[nrow];
+
+    /* Print the pattern */
+    printf("  Dimension: %lld-by-%lld (%lld nonzeros)\n", nrow, ncol, nnz);
+    printf("  Nonzeros: {");
+    casadi_int rr,cc,el;
+    for(cc=0; cc<ncol; ++cc){                    /* loop over columns */
+      for(el=colind[cc]; el<colind[cc+1]; ++el){ /* loop over the nonzeros entries of the column */
+        if(el!=0) printf(", ");                  /* Separate the entries */
+        rr = row[el];                            /* Get the row */
+        printf("{%lld,%lld}",rr,cc);                 /* Print the nonzero */
+      }
+    }
+    printf("}\n\n");
 
 	long i;
 	int_t nWSR;
---
+++ src/nlp_definition.py
@@ -0,0 +1,42 @@
+import casadi as ca 
+import numpy as np
+from os import system 
+
+nv = 2
+ni = 1
+
+y = ca.SX.sym('y', nv, 1)
+lam = ca.SX.sym('lam', ni, 1)
+
+opts = dict(with_header=True)
+
+# objective function 
+f = 1.0/2.0*(y[0]**2 + y[1]**2)
+
+ca_dfdy = ca.Function('ca_dfdy', [y], [ca.jacobian(f,y)])
+ca_dfdy.generate('ca_dfdy', opts)
+system('gcc -fPIC -shared -O3 ca_dfdy.c -o ../bin/libca_dfdy.so')
+system('gcc -fPIC -shared -O3 ca_dfdy.c -o ../bin/ca_dfdy.so')
+
+# constraints
+g = y[0] - y[1]**2 
+
+ca_g = ca.Function('ca_g', [y], [g])
+ca_g.generate('ca_g', opts)
+system('gcc -fPIC -shared -O3 ca_g.c -o ../bin/libca_g.so')
+system('gcc -fPIC -shared -O3 ca_g.c -o ../bin/ca_g.so')
+ca_g.save("../bin/ca_g.casadi")
+# temp = ca.Function.load("../bin/ca_g.casadi")
+# import pdb; pdb.set_trace()
+
+ca_dgdy = ca.Function('ca_dgdy', [y], [ca.jacobian(g,y)])
+ca_dgdy.generate('ca_dgdy', opts)
+system('gcc -fPIC -shared -O3 ca_dgdy.c -o ../bin/libca_dgdy.so')
+system('gcc -fPIC -shared -O3 ca_dgdy.c -o ../bin/ca_dgdy.so')
+
+# Lagrangian
+L = f + ca.dot(lam, g)
+
+ca_dLdyy = ca.Function('ca_dLdyy', [y, lam], [ca.hessian(L,y)[0]])
+ca_dLdyy.generate('ca_dLdyy', opts)
+system('gcc -fPIC -shared -O3 ca_dLdyy.c -o ../bin/libca_dLdyy.so')
