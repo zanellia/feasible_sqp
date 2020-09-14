6b5bc20aa6e79ac845075f2b05ab4c6ff8a886b1
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2020-09-14 11:56:40 +0200

    updat gen scirpt

--- benchmark/generate_nl_files.py
+++ benchmark/generate_nl_files.py
@@ -1,11 +1,35 @@
 import glob, os, string
 
-os.chdir("./mod_files")
+# os.chdir("./cuter/mod_files")
+# for file_name in glob.glob("*.mod"):
+#     name_ = file_name.split('.')[0]
+    
+#     print('converting ', file_name)
+#     os.system("../../../amplide.linux64/ampl -og{} {}".format(\
+#         name_, file_name))
+
+#     os.system("mv {} ../nl_files".format(name_ + '.nl'))
+
+# os.chdir("../..")
+
+os.chdir("./revisedhsmodels/processed/ampl/revisedhs")
 for file_name in glob.glob("*.mod"):
     name_ = file_name.split('.')[0]
     
-    print('converting ', file_name, '.mod')
-    os.system("../../../amplide.linux64/ampl -og{} {}".format(\
+    print('converting ', file_name)
+    os.system("../../../../../../amplide.linux64/ampl -og{} {}".format(\
         name_, file_name))
 
-    os.system("mv {} ../nl_files".format(name_ + '.nl'))
+    os.system("mv {} ../../nl/revisedhs".format(name_ + '.nl'))
+
+os.chdir("../../../..")
+
+os.chdir("./revisedhsmodels/processed/ampl/stricths")
+for file_name in glob.glob("*.mod"):
+    name_ = file_name.split('.')[0]
+    
+    print('converting ', file_name)
+    os.system("../../../../../../amplide.linux64/ampl -og{} {}".format(\
+        name_, file_name))
+
+    os.system("mv {} ../../nl/stricths".format(name_ + '.nl'))
