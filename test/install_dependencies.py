6afe1aabe54f6d1cd07c18d6938d5027cc88cb1d
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2021-04-21 13:14:08 +0200

    store matlab path to file

--- test/install_dependencies.py
+++ test/install_dependencies.py
@@ -1,7 +1,6 @@
 import sys
 from feasible_sqp import *
 
-matlab_root_path = sys.argv[1]
-
-install_dependencies(matlab_root_path=matlab_root_path)
-
+matlab_root_path_file = sys.argv[1]
+with open(matlab_root_path_file) as f:
+    matlab_root_path = f.read()
