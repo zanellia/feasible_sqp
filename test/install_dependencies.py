419121625bbee2793a2d394b10b04e1989b7f036
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2021-04-21 13:24:54 +0200

    readd call to install_dependencies()

--- test/install_dependencies.py
+++ test/install_dependencies.py
@@ -4,3 +4,5 @@ from feasible_sqp import *
 matlab_root_path_file = sys.argv[1]
 with open(matlab_root_path_file) as f:
     matlab_root_path = f.read()
+
+install_dependencies(matlab_root_path=matlab_root_path)
