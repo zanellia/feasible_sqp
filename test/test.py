2cb1c0980ce01b6fcce7bc6f65bce72f60ae435b
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2021-04-21 12:01:53 +0200

    fix test.py

--- test/test.py
+++ test/test.py
@@ -1,30 +1,35 @@
+import sys
 from feasible_sqp import *
 
-INSTALL_DEPS = 0
+matlab_root_path = sys.argv[1]
 
-if INSTALL_DEPS:
-    install_dependencies(matlab_lib_path='/usr/local/MATLAB/R2017b/bin/glnxa64',\
-        matlab_include_path='/usr/local/MATLAB/R2017b/extern/include')
+install_dependencies(matlab_root_path=matlab_root_path)
 
 # number of primal variables
 nv = 2
+
 # create solver
 solver = feasible_sqp(nv)
+
 # get primal variables
 y = solver.y
 
 # define cost
 f = 1.0/2.0*ca.dot(y-10,y-10)
+
 # define constraints
 g = ca.vertcat(ca.sin(y[1]) - y[0])
+
 # define bounds
 lby = -0.4*np.ones((nv,1))
 uby = 0.7*np.ones((nv,1))
+
 # define nonlinear constraints
 lbg = -0.0001*np.ones((1,1))
 ubg = 0.0001*np.ones((1,1))
+
 # generate solver
-# solver.generate_solver(f,g, lby = lby, uby = uby, lbg=lbg, ubg=ubg)
-solver.generate_solver(f,g, lby = lby, uby = uby)
+solver.generate_solver(f,g, lby = lby, uby = uby, lbg=lbg, ubg=ubg)
+
 # solve NLP
 solver.solve()
