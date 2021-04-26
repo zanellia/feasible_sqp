64fb4a3e795a9bdf659fe94bd87ac184276ca1b0
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2021-04-26 11:55:23 +0200

    set LIB_BLAS/LAPACK

---
+++ external/fsqp_solver/fsqp_solver.hpp
@@ -0,0 +1,25 @@
+extern "C" {
+int fsqp_solver( );
+}
+
+double lby[2] = {
+    
+    -0.4,
+    -0.4,
+};
+
+double uby[2] = {
+    
+    0.7,
+    0.7,
+};
+
+double lbg[1] = {
+    
+    0.0,
+};
+
+double ubg[1] = {
+    
+    0.0,
+};
