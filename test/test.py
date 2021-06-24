e4f6667f8ad5a0513d6967fbfed0fe3be9a2ac3f
andrea.zanelli <andrea.zanelli@imtek.uni-freiburg.de>
2021-06-24 11:17:06 +0200

    fix numpy import in test.py

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 from feasible_sqp import *
+import numpy as nmp
 
 # number of primal variables
 nv = 2
@@ -16,12 +17,12 @@ f = 1.0/2.0*ca.dot(y-10,y-10)
 g = ca.vertcat(ca.sin(y[1]) - y[0])
 
 # define bounds
-lby = -0.4*np.ones((nv,1))
-uby = 0.7*np.ones((nv,1))
+lby = -0.4*nmp.ones((nv,1))
+uby = 0.7*nmp.ones((nv,1))
 
 # define nonlinear constraints
-lbg = -0.0001*np.ones((1,1))
-ubg = 0.0001*np.ones((1,1))
+lbg = -0.0001*nmp.ones((1,1))
+ubg = 0.0001*nmp.ones((1,1))
 
 # generate solver
 # solver.generate_solver(f,g, lby = lby, uby = uby, lbg=lbg, ubg=ubg)
