9f7cce31a80a55097cdff929a64366c97cbe7e67
zanellia <zanellia@ethz.ch>
2021-11-26 18:37:11 +0100

    fix blas and lapack

--- test/install_dependencies.py
+++ test/install_dependencies.py
@@ -9,7 +9,4 @@ from feasible_sqp import *
 cwd = os.getcwd()
 # install_dependencies(matlab_root_path=matlab_root_path)
 install_dependencies(
-    blas_lib_path='/usr/lib/libblas.so', \
-    lapack_lib_path='/usr/lib/lapack/liblapack.so', \
-    # hsl_lib_path='/usr/local/MATLAB/R2017b/bin/glnxa64/libmwma57.so')
     hsl_lib_path= cwd + '/coin_hsl_source/.libs/libcoinhsl.so')
